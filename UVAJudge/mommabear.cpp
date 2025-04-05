4ddc34942d49ca7d23d2efc302067b656293671b
dataNerd65 <pkiarie811@gmail.com>
2025-04-05 17:59:51 +0300

    setup complete

--- UVAJudge/mommabear.cpp
+++ UVAJudge/mommabear.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
 #include<cctype>
+#include<string>
+#include<algorithm>
 
 std::string changeCaseAndremPun(std::string sentence){
 	//first to lowercase using a loop
 	for(char &c: sentence){
 		c = std::tolower(c);
 	}
+	
 	//REMOVING everything except 
+	sentence.erase(std::remove_if(sentence.begin(), sentence.end(), [](char c){
+		return !(c >= 'a' && c <= 'z');
+	}), sentence.end());
+
+	//std::cout<<sentence<<'\n';
 	return sentence;
 }
 
@@ -20,7 +28,18 @@ int main(){
 		sentence = changeCaseAndremPun(sentence);
 
 		// debugging
-		std::cout<<sentence<<'\n';
+		//std::cout<<sentence<<'\n';
+
+		// copy
+		std::string original = sentence;
+
+		std::reverse(sentence.begin(), sentence.end());
+
+		if(original == sentence){
+			std::cout<<"You won't be eaten!"<<'\n';
+		}else{
+			std::cout<<"Uh oh.."<<'\n';
+		}
 	}
 
 	return 0;
